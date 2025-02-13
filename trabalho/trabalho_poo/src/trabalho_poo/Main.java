bba66e483eedbfeb36912f12f7a466ecbf9859b0
Renato Martins <rrbarros1505@gmail.com>
2025-02-13 09:35:58 -0300

    Getters e setters na classe Navio

--- trabalho/trabalho_poo/src/trabalho_poo/Main.java
+++ trabalho/trabalho_poo/src/trabalho_poo/Main.java
@@ -3,7 +3,7 @@ package trabalho_poo;
 public class Main {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 
 	}
 
--- trabalho/trabalho_poo/src/trabalho_poo/Navio.java
+++ trabalho/trabalho_poo/src/trabalho_poo/Navio.java
@@ -7,7 +7,80 @@ public abstract class Navio {
     private String portoOrigem;
     private String portoDestino;
     private String pais;
-    private int id;
+    private int id; // nao fiz ainda
     private Capitao capitao;
     private String sentido;
-}
+
+    public abstract void setComprimento(); 
+
+    public double getComprimento(){
+        return comprimento;
+    }
+
+    public abstract void setLargura(); 
+
+    public double getLargura(){
+        return largura;
+    }
+
+    public void setCargaMaxima(); 
+
+    public double getCargaMaxima(){
+        return cargaMaxima;
+    }
+
+    public void setPortoOrigem(String portoOrigem){
+        if(portoOrigem != null){
+            this.portoOrigem = portoOrigem;
+        }
+    }
+
+    public String getPortoOrigem(){
+        return portoOrigem;
+    }
+
+    public void setPortoDestino(String portoDestino){
+        if(portoDestino != null){
+            this.portoDestino = portoDestino;
+        }
+    }
+
+    public String getPortoDestino(){
+        return portoDestino;
+    }
+
+    public void setPais(String pais){
+        if(pais != null){
+            this.pais = pais;
+        }
+    }
+
+    public String getPais(){
+        return pais;
+    }
+
+    //public void setId()
+
+    //public int getId()
+
+    public void setCapitao(Capitao capitao){
+        if(capitao != null){
+            this.capitao = capitao;
+        }
+    }
+
+    public Capitao getCapitao(){
+        return capitao;
+    }
+
+    public void setSentido(String sentido){
+        if(sentido != null && (sentido == "rio_mar" || sentido == "mar_rio")){
+            this.sentido = sentido;
+        }
+    }
+
+    public String getSentido(){
+        return sentido;
+    }
+}   
+
