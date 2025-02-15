5ade416fa8e7cd3171f754eac98e4e10b8aa456d
Daniel A. Gonçalves de Oliveira <86572380+DanielOliveira2@users.noreply.github.com>
2025-02-15 15:40:28 -0300

    Update Passageiro.java

--- trabalho/src/model/Passageiro.java
+++ trabalho/src/model/Passageiro.java
@@ -2,4 +2,17 @@ package model;
 
 public class Passageiro extends Pessoa {
     private String idPassagem;
+
+    public Passageiro(String nome, int cpf, String idPassagem) {
+        super(nome, cpf);
+        this.idPassagem = idPassagem;
+    }
+
+    public String getIdPassagem() {
+        return idPassagem;
+    }
+
+    public void setIdPassagem(String idPassagem) {
+        this.idPassagem = idPassagem;
+    }
 }
