7accff327443911607be5fbc453b663e932ffb83
Daniel A. Gonçalves de Oliveira <86572380+DanielOliveira2@users.noreply.github.com>
2025-02-15 14:02:38 -0300

    Update Rio

--- trabalho/src/model/Rio.java
+++ trabalho/src/model/Rio.java
@@ -1,5 +1,25 @@
 package model;
 
-public class Rio{
+public class Rio {
     private double nivelDoRio;
+
+    public Rio(double nivelDoRio) {
+        if (nivelDoRio > 0) { // O nível do rio deve ser maior que 0
+            this.nivelDoRio = nivelDoRio;
+        } else {
+            throw new IllegalArgumentException("Nível do rio inválido. Deve ser maior que zero.");
+        }
+    }
+
+    public double getNivelDoRio() {
+        return nivelDoRio;
+    }
+
+    public void setNivelDoRio(double nivelDoRio) {
+        if (nivelDoRio > 0) { // Mantendo a restrição
+            this.nivelDoRio = nivelDoRio;
+        } else {
+            throw new IllegalArgumentException("Nível do rio inválido. Deve ser maior que zero.");
+        }
+    }
 }
