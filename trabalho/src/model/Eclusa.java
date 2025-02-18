744da6c549e2ed2f488a3f618df8b7d4bb365711
Renato Martins <rrbarros1505@gmail.com>
2025-02-18 20:54:00 -0300

    Conclusões

--- trabalho/src/model/Eclusa.java
+++ trabalho/src/model/Eclusa.java
@@ -6,9 +6,9 @@ public class Eclusa{
     private double nivelDaEclusa;
     private double capacidadeMaxima;
     private double capacidadeMinima;
-    private double vazao;
-    //private double tempoParaEncher;
-    //private double preco; // depende do tipo da embarcação
+
+    private boolean temNavio = false;
+    private Navio navio;
 
     public void setComprimento(double comprimento){
         if(comprimento >= 0 && comprimento <= 300){
@@ -66,9 +66,45 @@ public class Eclusa{
         return capacidadeMinima;
     }
 
-    public void setVazao(double vazao){
-        if(vazao > 0){
-            this.vazao = vazao;
+    public void setTemNavio(boolean temNavio){
+        this.temNavio = temNavio;
+    }
+
+    public boolean getTemNavio(){
+        return temNavio;
+    }
+    
+    public void setNavio(Navio navio){
+        if(navio != null){
+            this.navio = navio;
+        }
+    }
+
+    public Navio getNavio(){
+        return navio;
+    }
+
+    public String addNavioNaEclusa(Navio navio){
+        if(navio != null && getTemNavio() == false){
+            setNavio(navio);
+            setTemNavio(true);
+            return "Navio adicionado na eclusa\n";
+        }
+        else if(getTemNavio() == true){
+            return "Ja existe um navio na eclusa\n";
+        }
+    }
+
+    public String realizarPassagemDoNavio(){
+        if(getTemNavio() == true && getNavio() != null){
+            if(getNavio().getSentido() == "rio_mar"){
+                return "A embarcacao passou do Rio para o Mar\n";
+            }
+            else if(getNavio().getSentido() == "mar_rio"){
+                return "A embarcacao passou do Mar para o Rio\n";
+            }
+            setTemNavio(false);
+            setNavio(null);
         }
     }
 
